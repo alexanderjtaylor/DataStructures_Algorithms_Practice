f94875d3fb86f6a573eb25b5fe054dbf10d0b4ec
Alex <alexandertaylor315@gmail.com>
2023-04-27 17:20:04 -0400

    leetcode-235.py solves problem of finding the lowest common ancestor through use of recursion + leetcode-100.py solves the question if two binary trees are the same

---
+++ binary_trees/leetcode-100.py
@@ -0,0 +1,20 @@
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def isSameTree(self, p, q) -> bool:
+        if p is None and q is None:
+            return True
+        elif p is None or q is None:
+            return False
+        else:
+            if p.val == q.val:
+                return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            else:
+                return False
