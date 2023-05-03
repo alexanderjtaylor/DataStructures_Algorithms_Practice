ed5d413e1bebef5ffdbc2f88443efe97ded73426
Alex <alexandertaylor315@gmail.com>
2023-05-02 23:40:26 -0400

    created leetcode-383.py with solution to see if ransonnote can be constructed by using letters from magazine. To find this out we iterate through each letter in ransomnote and if the letter is greater than the letter in magazine then it will be false, otherwise it will be true

---
+++ grind75/leetcode-383.py
@@ -0,0 +1,8 @@
+
+
+class Solution:
+    def canConstruct(self, ransomNote, magazine):
+        for i in set(ransomNote):
+            if magazine.count(i) < ransomNote.count(i):
+                return False
+        return True
