456be71591207248a9379182c7beca562eccc8d1
Alex <alexandertaylor315@gmail.com>
2023-05-22 05:34:06 -0400

    updated leetcode-543.py file with easier to understand solution. By using a helper function we can recursively search through left and right nodes and updating ans to the max diameter found by adding left and right subtrees together then returning max height plus one gives diameter

--- binary_trees/leetcode-543.py
+++ binary_trees/leetcode-543.py
@@ -1,16 +1,39 @@
 
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# class Solution:
+#     def diameterOfBinaryTree(self, node):
+#         res = 0
+#         def height(node):
+#             nonlocal res
+#             if node is None:
+#                 return -1
+#             else:
+#                 left = height(node.left)
+#                 right = height(node.right)
+#                 res = max(res, 2 + left + right)
+#             return 1 + max(left, right)
+#         height(node)
+#         return res
+
+
 class Solution:
-    def diameterOfBinaryTree(self, node):
-        res = 0
-        def height(node):
-            nonlocal res
-            if node is None:
-                return -1
-            else:
-                left = height(node.left)
-                right = height(node.right)
-                res = max(res, 2 + left + right)
-            return 1 + max(left, right)
-        height(node)
-        return res
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.ans = 0  # variable to store the maximum diameter found so far
+        
+        def dfs(node):
+            if not node:
+                return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+            self.ans = max(self.ans, left + right)  # update ans if new diameter is found
+            return max(left, right) + 1  # return the maximum height of the node
+            
+        dfs(root)
+        return self.ans
